28f8a34fe195aff27c926001041181f05147020c
3033593256@qq.com <3033593256@qq.com>
2016-09-16 23:39:19 +0800

    it is incipient stage

---
+++ 1--同步到打包目录.bat
@@ -0,0 +1,5 @@
+cd D:\360files\1_Crack\3_apktool2.2\YOU
+rd /q /s smali\
+md   "D:\360files\1_Crack\3_apktool2.2\YOU\smali"
+xcopy "D:\CODE\APK\APKDebug\smalicode\src\smali" "D:\360files\1_Crack\3_apktool2.2\YOU\smali" /e
+exit
--- 2--启动-APP.bat
+++ 2--启动-APP.bat
@@ -1,5 +1,5 @@
 cd D:\360files\1_Crack\3_apktool2.2
 
-adb shell am start -D -W -n com.tianyi.kxmmx.kuaiya/com.tianyi.kxmmx.kuaiya.AndroidLauncher
+adb shell am start -D -W -n com.yunyou.bjkr.apk/com.yunyou.bjkr.apk.tiantianbaoji
 
 exit
